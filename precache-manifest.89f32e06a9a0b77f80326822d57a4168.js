6949e7b1d301931538de9cd0d8a5a7fa9394bb1b
Thomas Mery <thomas.mery@gmail.com>
2019-11-11 18:40:05 +0100

    Updates

---
+++ precache-manifest.89f32e06a9a0b77f80326822d57a4168.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a3d242f4ab24d7b653d318181b9ac6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2baafc7ce713f27ba071",
+    "url": "/static/css/2.b883d912.chunk.css"
+  },
+  {
+    "revision": "eb56b13917336ac1e032",
+    "url": "/static/css/main.b22ef909.chunk.css"
+  },
+  {
+    "revision": "2baafc7ce713f27ba071",
+    "url": "/static/js/2.b8dffc0b.chunk.js"
+  },
+  {
+    "revision": "eb56b13917336ac1e032",
+    "url": "/static/js/main.bebeb2e5.chunk.js"
+  },
+  {
+    "revision": "e8f37508c40b0497f902",
+    "url": "/static/js/runtime-main.62fd6a51.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
